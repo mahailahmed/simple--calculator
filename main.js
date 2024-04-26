53951ca18fe38899ba944fc252971c85067cc13a
mahailahmed <160250995+mahailahmed@users.noreply.github.com>
2024-04-25 18:01:11 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+#! /user/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter first number", type: "number", name: "firstNumber" },
+    { message: "Enter second number", type: "number", name: "secondNumber" },
+    { message: "select one of the oprators to perform action",
+        type: "list",
+        name: "oprator",
+        choices: ["addition", "subtraction", "multiplication", "division"],
+    },
+]);
+//condiotional statement
+if (answer.oprator === "addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.oprator === "subtraction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.oprator === "multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.oprator === "division") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else {
+    console.log("please select valid opraotor");
+}
